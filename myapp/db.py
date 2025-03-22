d2a5ec223469c56ec7d2421582e9886ed854adf1
piemahesh <piemahesh@gmail.com>
2025-03-22 17:33:29 +0530

    new changes

--- myapp/db.py
+++ myapp/db.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 
 client = MongoClient("mongodb+srv://sivaramakrishnan9952614001:NAMSyRv88taYjp0r@cluster0.5pjy7.mongodb.net/")
+name = "hello"
 db = client.krishnan
 col2 = db.col2  # Ensure col2 is correctly defined
